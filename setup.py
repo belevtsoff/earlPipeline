b918c8d67e2aae0a8aefc242de3f9d720c45c0ed
belevtsoff <belevtsoff@gmail.com>
2014-07-17 14:04:05 +0200

    added packaging scripts

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='earlPipeline',
+    version='0.0.1',
+    author='Dmytro Bielievtsov',
+    author_email='belevtsoff@gmail.com',
+    packages=['earlpipeline', 'earlpipeline.backends'],
+    #url='http://pypi.python.org/pypi/TowelStuff/',
+    license='LICENSE',
+    description='A simplistic backend-agnostic web application for visualizing and constructing modular data-processing pipelines',
+    long_description=open('README.md').read(),
+    include_package_data=True,
+    install_requires=[
+        "tornado",
+        "logutils",
+        "bidict >= 0.1.2"
+    ],
+)
